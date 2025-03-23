79f5fbfdeb44ab95d991e61862d31f313317fee5
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2025-03-23 12:54:00 +0900

    Time: 0 ms (100%), Space: 17.9 MB (14.17%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -1,10 +1,8 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         count = 0
-        while num1 != 0 and num2 != 0:
-            if num1 >= num2:
-                num1 -= num2
-            else:
-                num2 -= num1
-            count += 1
+        num1,num2 = max(num1,num2),min(num1,num2)
+        while num2 != 0:
+            count += num1//num2
+            num1,num2 = num2,num1%num2
         return count 
