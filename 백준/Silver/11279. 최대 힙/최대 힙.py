ea568034ce83e99bb94ce21a3bab45a2e104a38a
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2025-09-01 23:27:48 +0800

    [Silver II] Title: 최대 힙, Time: 132 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,13 @@
+import sys
+import heapq as hp
+arr = []
+N = int(sys.stdin.readline().strip())
+for _ in range(N):
+    num = int(sys.stdin.readline().strip())
+    if num == 0:
+        if len(arr) == 0:
+            print(0)
+        else:
+            print(abs(hp.heappop(arr)))
+    else:
+        hp.heappush(arr,-num)
