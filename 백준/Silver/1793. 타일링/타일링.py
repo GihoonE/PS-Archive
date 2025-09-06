f3e62cb5e659e2c57fa7acc816f2ae641101b0b6
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2025-09-06 19:20:13 +0800

    [Silver II] Title: 타일링, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,11 @@
+import sys
+dp = [0]*251
+dp[0] = 1
+dp[1] = 1
+dp[2] = 3
+for i in range(3,251):
+    dp[i] = dp[i-1]+2*dp[i-2]
+lines = sys.stdin.readlines()
+for line in lines:
+    N = int(line.strip())
+    print(dp[N])
