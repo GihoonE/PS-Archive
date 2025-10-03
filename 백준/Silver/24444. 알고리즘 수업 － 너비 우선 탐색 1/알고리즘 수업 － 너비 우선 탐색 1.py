102324cd33aa5f8f2fc386572e9b95cb2969262a
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2025-10-03 20:52:54 +0800

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 768 ms, Memory: 61788 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+V,E,S = map(int,sys.stdin.readline().strip().split())
+arr = [[] for _ in range(V+1)]
+for _ in range(E):
+    a,b = map(int,sys.stdin.readline().strip().split())
+    arr[a].append(b)
+    arr[b].append(a)
+visited = [False]*(V+1)
+deque = deque()
+deque.append(S)
+ind = 1
+while deque:
+    current_node = deque.popleft()
+    if visited[current_node] == 0:
+        visited[current_node] = ind
+        ind += 1
+        adjs = sorted(arr[current_node])
+        for nn in adjs:
+            deque.append(nn)
+
+for i in range(1,len(visited)):
+    if visited[i]:
+        print(visited[i])
+    else:
+        print(0)
