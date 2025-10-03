de1a3502c01e68db1b37f8a2c296dde246423a49
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2025-10-03 20:49:40 +0800

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 588 ms, Memory: 62316 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+V,E,S = map(int,sys.stdin.readline().strip().split())
+arr = [[] for _ in range(V+1)]
+for _ in range(E):
+    a,b = map(int,sys.stdin.readline().strip().split())
+    arr[a].append(b)
+    arr[b].append(a)
+visited = [False]*(V+1)
+stack = [S]
+ind = 1
+while stack:
+    current_node = stack.pop()
+    if visited[current_node] == 0:
+        visited[current_node] = ind
+        ind += 1
+        adjs = sorted(arr[current_node])
+        # print(f'{current_node} adjs: {adjs}')
+        for next_node in adjs:
+            stack.append(next_node)
+
+for i in range(1,len(visited)):
+    if visited[i]:
+        print(visited[i])
+    else:
+        print(0)
