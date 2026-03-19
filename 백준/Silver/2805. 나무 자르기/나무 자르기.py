49f33c9337477f04a9fdf8f04402e1a85e984213
Gihun Lee <69286786+GihoonE@users.noreply.github.com>
2026-03-19 19:10:55 +0800

    [Silver II] Title: 나무 자르기, Time: 2752 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,27 +1,19 @@
 import sys
 input = sys.stdin.readline
-
 N, T = map(int, input().split())
-arr = list(map(int, input().split()))
-
-lo_h = 0
+arr = list(map(int,input().split()))
 up_h = max(arr)
-ans = 0
-
+lo_h = 0
 while lo_h <= up_h:
-    mid = (lo_h + up_h) // 2
+    mid = (up_h + lo_h) // 2
     cur = 0
-
     for l in arr:
         if l > mid:
-            cur += l - mid
-            if cur >= T:
-                break
-
-    if cur >= T:
-        ans = mid
-        lo_h = mid + 1
-    else:
+            cur += l-mid
+        if cur >= T:
+            lo_h = mid + 1
+            break
+    if cur < T:
         up_h = mid - 1
-
-print(ans)
+    
+print((up_h + lo_h)//2)
